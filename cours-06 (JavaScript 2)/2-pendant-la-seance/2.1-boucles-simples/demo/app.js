ccefc77dd136c8b87493e40440dc04d6287404fe
Daniel Parpal <daniel.parpal@shopify.com>
2021-02-22 10:19:42 -0500

    Matériel cours 8

---
+++ cours-06 (JavaScript 2)/2-pendant-la-seance/2.1-boucles-simples/demo/app.js
@@ -0,0 +1,28 @@
+// Affichage des nombres de 1 à 10
+for (var i = 1; i <= 10; i = i + 1) {
+	console.log(i);
+}
+
+console.log('==========');
+console.log('A');
+console.log('B');
+
+for (var j = 1; j < 5; j = j + 1) {
+	console.log('Bonjour', j);
+}
+
+console.log('==========');
+
+// 3, 4, 5, 6
+for (var k = 3; k < 7; k = k + 1) {
+	console.log("Allô");
+}
+
+console.log('==========');
+
+// 1, 4, 7, 10, 13, 
+for (var a = 1; a <= 14; a = a + 3) {
+	console.log(a);
+}
+
+console.log(a); // 16
---
+++ cours-06 (JavaScript 2)/2-pendant-la-seance/2.2-parcourir-un-tableau/demo/app.js
@@ -0,0 +1,22 @@
+// Idée 1: Définir un tableau
+var produits = [
+	"Chandail Nemma de Prana - Femmes",
+	"Couche intermédiaire à demi-glissière Rockwall de MEC - Femmes",
+	"Chandail à capuchon Campshire 2.0 de The North Face - Femmes",
+	"Cardigan Covert de Arc'teryx - Femmes"
+];
+
+// Idée 2: Accéder à un élément spécifique
+console.log(produits[2]);
+console.log('=====');
+
+// Idée 3: Déterminer la quantité d'éléments dans le tableau
+console.log(produits.length);
+console.log('=====');
+
+// Idée 4: Utiliser la boucle dans sa forme la plus simple
+// Idée 5: RENDRE L'INDEX DYNAMIQUE
+// Idée 6: Rendre la condition de sortie dynamique
+for (var i = 0; i < produits.length; i = i + 1) {
+	console.log(produits[i]);
+}
